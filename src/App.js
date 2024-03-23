bcbc5574e2cd38a5cbc8a8ff76b5e1a6b951e884
josoyean <dlfjswhtnals@naver.com>
2024-03-23 13:35:34 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
           <Route path="/mypage" element={<MyPage />}></Route>
           <Route path="/account" element={<Account />}></Route>
           <Route
-            path="/oauth/kakao"
+            path="/oauth/login/KAKAO"
             // path="/oauth/redirected/kakao"
             element={<KakoRedirectPage />}
           />
